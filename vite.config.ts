4a96ca76d51db79b6797c4c5e3f9aba3286563cb
3jun <kk3june@gmail.com>
2025-05-02 01:52:02 +0900

    feat. 배포 setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-
+import { resolve } from "path"
+import { defineConfig, Plugin } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), apiReplace()],
+  base: "/front_5th_chapter2-3/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+  },
+
   server: {
     proxy: {
       "/api": {
@@ -15,3 +25,17 @@ export default defineConfig({
     },
   },
 })
+
+function apiReplace(): Plugin {
+  return {
+    name: "api-replace",
+    apply: "build",
+    transform(code, id) {
+      if (!id.match(/\.(ts|js|tsx|jsx)$/)) return
+      return code.replace(
+        /(['"`])\/api([^"'`\\]*)\1/g,
+        (_, quote, apiPath) => `${quote}https://dummyjson.com${apiPath}${quote}`,
+      )
+    },
+  }
+}
